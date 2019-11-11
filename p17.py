0c712d9b879d9a165618240e689b6669daa4570d
afarica <afaricanistan@gmail.com>
2019-11-12 02:05:39 +0600

    8 task

---
+++ p17.py
@@ -0,0 +1,15 @@
+a=input("Enter the name or place of the marathon participant:")
+if a==1:
+	print("Usain Bolt")
+elif a=="Usain Bolt":
+	print("1")
+elif a==2:
+	print("Avtandil")
+elif a=="Avtandil":
+	print("2")
+elif a==3:
+	print("Aibek")
+elif a=="Aibek":
+	print("3")
+else: 
+	print("There is no such member in our data!")
